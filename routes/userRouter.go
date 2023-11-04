07a60b08f13e58715e22ed1c9834ccb2c1ff20ed
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-05 01:01:45 +0600

    User Router

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"restaurent-mng-bc/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(router *gin.Engine) {
+	// register routes
+	router.GET("/user/", controllers.GetUsers)
+	router.POST("/user/sign-up/", controllers.CreateUserRouterHandler)
+	router.POST("/user/login/", controllers.LoginUser)
+	router.PUT("/user/:id", controllers.UpdateUserRouterByIDHandler)
+	router.DELETE("/user/:id", controllers.DeleteUserRouterByIDHandler)
+
+}
