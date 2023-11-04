5042b2b4c2b476ea49f98c90c04af5133d454e9f
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-05 01:01:26 +0600

    Feat(User Controller): UC created

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,21 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers(c *gin.Context) {
+
+}
+
+func CreateUserRouterHandler(c *gin.Context) {
+
+}
+func UpdateUserRouterByIDHandler(c *gin.Context) {
+
+}
+func DeleteUserRouterByIDHandler(c *gin.Context) {
+
+}
+
+func LoginUser(c *gin.Context) {
+
+}
