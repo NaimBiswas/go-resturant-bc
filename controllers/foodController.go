07cef2368d6d0a6d700d5c4686e30dfa917223a0
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-06 10:36:17 +0600

    Feat(FC): Food get api

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,14 +1,31 @@
 package controllers
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+	"restaurent-mng-bc/database"
+	"restaurent-mng-bc/response"
+	"time"
 )
 
-//var collection = "foods"
-//var _Db = database.DbInstance()
+var foodCollection string = "foods"
 
 func GetFoods(c *gin.Context) {
+	var _Db = database.Db
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	result, err := _Db.Collection(foodCollection).Find(context.TODO(), bson.M{})
+	if err != nil {
+		response.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+	}
+	var users []bson.M
 
+	if err = result.All(ctx, &users); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
+	response.SuccessResponse(c, http.StatusAccepted, users)
 }
 func GetFood(c *gin.Context) {
 
