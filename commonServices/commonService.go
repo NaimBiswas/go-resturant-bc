247c52d7530d74c8bc917c1342e4a83b9a0c9ac2
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-06 10:34:30 +0600

    Created(CS):Common Service

---
+++ commonServices/commonService.go
@@ -0,0 +1,26 @@
+package commonServices
+
+import (
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+	"time"
+)
+
+func GetDate() time.Time {
+	date, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	return date
+}
+
+func ObjectId(id string) primitive.ObjectID {
+	ObjectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal("Error In Converting ObjectId", err.Error())
+	}
+	return ObjectId
+}
+
+func GenerateCode() string {
+	code := fmt.Sprint(time.Now().Nanosecond())
+	return code[:6]
+}
