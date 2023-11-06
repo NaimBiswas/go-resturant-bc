4f2e775f0df956c2dbdf39ff8bb36b3e6e881d0b
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-06 10:37:06 +0600

    user and menu controller Api's

--- controllers/memuController.go
+++ controllers/memuController.go
@@ -1,17 +1,84 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"net/http"
+	"restaurent-mng-bc/database"
+	"restaurent-mng-bc/models"
+	"restaurent-mng-bc/response"
+	"time"
+)
 
-func CreateMenu(ctx *gin.Context) {
+var menuCollection = "menus"
 
+func CreateMenu(c *gin.Context) {
+	Db := database.Db
+	validate := validator.New()
+	var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var menu models.MenuModel
+	if err := c.BindJSON(&menu); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	validateErr := validate.Struct(menu)
+	if validateErr != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, validateErr.Error())
+		return
+	}
+	menu.ID = primitive.NewObjectID()
+	menu.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	menu.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	//check the validation for title
+	count, _ := Db.Collection(menuCollection).CountDocuments(ctx, bson.M{"name": menu.Name})
+	if count > 0 {
+		response.ErrorResponse(c, http.StatusBadRequest, "Menu with the same name already exits.")
+		return
+	}
+	_, err := Db.Collection(menuCollection).InsertOne(ctx, menu)
+	if err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	response.SuccessResponse(c, http.StatusCreated, "Menu has been Created Successfully")
 }
 
-func GetMenus(ctx *gin.Context) {
+func GetMenus(c *gin.Context) {
+	var _Db = database.Db
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	result, err := _Db.Collection(menuCollection).Find(context.TODO(), bson.M{})
+	if err != nil {
+		response.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var allMenus []bson.M
 
+	if err = result.All(ctx, &allMenus); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	response.SuccessResponse(c, http.StatusAccepted, allMenus)
 }
 
-func GetMenu(ctx *gin.Context) {
-
+func GetMenu(c *gin.Context) {
+	var _Db = database.Db
+	var menuId = c.Param("id")
+	ObjectId, _ := primitive.ObjectIDFromHex(menuId)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	var menu bson.M
+	err := _Db.Collection(menuCollection).FindOne(ctx, bson.M{"_id": ObjectId}).Decode(&menu)
+	if err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	response.SuccessResponse(c, http.StatusAccepted, menu)
 }
 
 func UpdateMenu(ctx *gin.Context) {
--- controllers/userController.go
+++ controllers/userController.go
@@ -1,19 +1,98 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"restaurent-mng-bc/commonServices"
+	"restaurent-mng-bc/database"
+	"restaurent-mng-bc/models"
+	"restaurent-mng-bc/response"
+	"time"
 )
 
-//var collection string = "users"
-//var _Db *mongo.Database = database.DbInstance()
+var userCollection string = "users"
 
 func GetUsers(c *gin.Context) {
+	var _Db = database.Db
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	result, err := _Db.Collection(userCollection).Find(context.TODO(), bson.M{})
+	if err != nil {
+		response.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var allMenus []bson.M
 
+	if err = result.All(ctx, &allMenus); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	response.SuccessResponse(c, http.StatusAccepted, allMenus)
+	return
+}
+
+func GetUser(c *gin.Context) {
+	var _Db = database.Db
+	var id string = c.Param("id")
+	ObjectId := commonServices.ObjectId(id)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	var user bson.M
+	err := _Db.Collection(userCollection).FindOne(ctx, bson.M{"_id": ObjectId}).Decode(&user)
+	if err != nil {
+		response.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	response.SuccessResponse(c, http.StatusAccepted, user)
+	return
 }
 
 func CreateUserRouterHandler(c *gin.Context) {
+	Db := database.Db
+	validate := validator.New()
+	var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var user models.UserModel
+	if err := c.BindJSON(&user); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	validateErr := validate.Struct(user)
+	if validateErr != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, validateErr.Error())
+		return
+	}
+	user.Password = HashPassword(user.Password)
+	user.ID = primitive.NewObjectID()
+	user.CreatedAt = commonServices.GetDate()
+	user.UpdatedAt = commonServices.GetDate()
+	user.VerificationCode = commonServices.GenerateCode()
+	user.IsActive = true
+	user.Status = "sign-up"
+
+	//check the validation for title
+	count, _ := Db.Collection(userCollection).CountDocuments(ctx, bson.M{"userName": user.UserName})
+	if count > 0 {
+		response.ErrorResponse(c, http.StatusBadRequest, "User with the same name already exits.")
+		return
+	}
+	_, err := Db.Collection(userCollection).InsertOne(ctx, user)
+	if err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	sendVerificationMail(user)
+	response.SuccessResponse(c, http.StatusCreated, "User has been Created Successfully")
 
 }
+
 func UpdateUserRouterByIDHandler(c *gin.Context) {
 
 }
@@ -25,10 +104,22 @@ func LoginUser(c *gin.Context) {
 
 }
 
-func hasPassword(password string) {
-
+func HashPassword(password string) string {
+	pw := []byte(password)
+	result, err := bcrypt.GenerateFromPassword(pw, bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return string(result)
 }
 
-func comparePassword(userPass string, dbHas string) bool {
-	return true
+func ComparePassword(hashPassword string, password string) error {
+	pw := []byte(password)
+	hw := []byte(hashPassword)
+	err := bcrypt.CompareHashAndPassword(hw, pw)
+	return err
+}
+
+func sendVerificationMail(user models.UserModel) {
+	fmt.Println("Mail Will be send from Here")
 }
