41ad84f92322a88a4f10eb73a971a6b34417fc6d
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-10 22:50:55 +0600

    Create user db Index

--- database/dbConnection.go
+++ database/dbConnection.go
@@ -3,12 +3,14 @@ package database
 import (
 	"context"
 	"fmt"
+	"log"
+	"restaurent-mng-bc/commonType/collections"
+	"restaurent-mng-bc/config"
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
-	"restaurent-mng-bc/config"
-	"time"
 )
 
 var Db *mongo.Database
@@ -36,13 +38,17 @@ func CreatedIndexes() {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	menuIndexModel := mongo.IndexModel{
-		Keys: bson.D{{"name", 1}},
+		Keys: bson.D{{Key: "name", Value: 1}},
 	}
 	foodIndexModel := mongo.IndexModel{
-		Keys: bson.D{{"name", 1}, {"menuId", 1}},
+		Keys: bson.D{{Key: "name", Value: 1}, {Key: "menuId", Value: 1}},
 	}
-	_, err := Db.Collection("menus").Indexes().CreateOne(ctx, menuIndexModel)
-	_, err = Db.Collection("foods").Indexes().CreateOne(ctx, foodIndexModel)
+	userIndexModel := mongo.IndexModel{
+		Keys: bson.D{{Key: "email", Value: 1}, {Key: "userName", Value: 1}, {Key: "isActive", Value: 1}},
+	}
+	_, err := Db.Collection(collections.MenuCollection).Indexes().CreateOne(ctx, menuIndexModel)
+	_, err = Db.Collection(collections.FoodCollection).Indexes().CreateOne(ctx, foodIndexModel)
+	_, err = Db.Collection(collections.UserCollection).Indexes().CreateOne(ctx, userIndexModel)
 
 	if err != nil {
 		log.Fatal(err.Error())
