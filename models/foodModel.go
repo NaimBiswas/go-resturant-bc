b792bbca132f376d20d1aa72dec444561ac0adc2
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-14 18:34:57 +0600

    Added bson in model for mongodb

--- models/foodModel.go
+++ models/foodModel.go
@@ -6,11 +6,12 @@ import (
 )
 
 type FoodModel struct {
-	ID        primitive.ObjectID `bson:"_id"`
-	Name      string             `json:"name" validate:"required,min=2max=100"`
-	Price     float64            `json:"price" validate:"required"`
-	CreatedAt time.Time          `json:"createdAt"`
-	UpdatedAt time.Time          `json:"updatedAt"`
-	FoodImage string             `json:"foodImage" validate:"required"`
-	MenuId    string             `json:"menuId" validated:"required"`
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	Name      string             `json:"name" bson:"name" validate:"required,min=2,max=100"`
+	Price     float64            `json:"price" validate:"required" bson:"price"`
+	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
+	FoodImage string             `json:"foodImage" validate:"required" bson:"foodImage"`
+	MenuId    string             `json:"menuId" validated:"required" bson:"menuId"`
+	IsActive  bool               `json:"isActive" bson:"isActive" bson:"isActive"`
 }
