8046dc3c210c6ededc9a0eecbff7e9c4783c9acd
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-16 11:01:22 +0600

    chore(Foods): Get All Post Get by Id api with Redis TODO: Need to use redis as a middle ware

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -2,38 +2,116 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"restaurent-mng-bc/commonServices"
+	"restaurent-mng-bc/commonType/collections"
+	"restaurent-mng-bc/database"
+	"restaurent-mng-bc/models"
+	"restaurent-mng-bc/response"
+	"strconv"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"net/http"
-	"restaurent-mng-bc/database"
-	"restaurent-mng-bc/response"
-	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-var foodCollection string = "foods"
 
 func GetFoods(c *gin.Context) {
 	var _Db = database.Db
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
-	result, err := _Db.Collection(foodCollection).Find(context.TODO(), bson.M{})
+
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+	pageNumber, _ := strconv.Atoi(c.Query("page"))
+	skip := (pageNumber - 1) * pageSize
+	findOptions := options.Find()
+	findOptions.SetLimit(int64(pageSize))
+	findOptions.SetSkip(int64(skip))
+
+	redisKey := collections.FoodCollection + ":" + c.Query("page") + ":" + c.Query("pagesize")
+
+	var foods []bson.M
+	err := commonServices.GetRedisStringValue(ctx, redisKey, &foods)
+
+	if err == nil {
+		response.SuccessResponse(c, http.StatusOK, foods)
+		return
+	}
+
+	result, err := _Db.Collection(collections.FoodCollection).Find(context.TODO(), bson.D{}, findOptions)
 	if err != nil {
 		response.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 	}
-	var users []bson.M
 
-	if err = result.All(ctx, &users); err != nil {
+	if err = result.All(ctx, &foods); err != nil {
 		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
 	}
-	response.SuccessResponse(c, http.StatusAccepted, users)
-}
-func GetFood(c *gin.Context) {
 
+	response.SuccessResponse(c, http.StatusOK, foods)
+	commonServices.SetStringVal(ctx, redisKey, foods, 120)
+}
+
+func GetFood(c *gin.Context) {
+	var _Db = database.Db
+	var foodId = c.Param("id")
+	fmt.Println(foodId)
+	ObjectId, _ := primitive.ObjectIDFromHex(foodId)
+	redisKey := collections.FoodCollection + ":" + foodId
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var food models.FoodModel
+
+	err := commonServices.GetRedisStringValue(ctx, redisKey, &food)
+	if err == nil {
+		log.Println(food)
+		response.SuccessResponse(c, http.StatusOK, food)
+		return
+	}
+
+	err = _Db.Collection(collections.FoodCollection).FindOne(ctx, bson.M{"_id": ObjectId}).Decode(&food)
+	if err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	response.SuccessResponse(c, http.StatusOK, food)
+	commonServices.SetStringVal(ctx, redisKey, food, 120)
 }
 
 func CreateFood(c *gin.Context) {
+	Db := database.Db
+	var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	var food models.FoodModel
+	if err := c.BindJSON(&food); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	validateErr := commonServices.Validate.Struct(food)
+	if validateErr != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, validateErr.Error())
+		return
+	}
 
+	food.ID = primitive.NewObjectID()
+	food.CreatedAt = commonServices.GetDate()
+	food.UpdatedAt = commonServices.GetDate()
+	food.IsActive = true
+
+	_, err := Db.Collection(collections.FoodCollection).InsertOne(ctx, food)
+	if err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	sendAlert(food)
+	response.SuccessResponse(c, http.StatusCreated, map[string]interface{}{"message": "Food has been Created Successfully", "food": food})
 }
+
 func UpdateFood(c *gin.Context) {
 
 }
@@ -41,10 +119,14 @@ func DeleteFood(c *gin.Context) {
 
 }
 
-func route(num float64) {
+func round(num float64) {
 
 }
 
 func toFixed(num float64, precision int) float64 {
 	return 0.0
+}
+
+func sendAlert(food models.FoodModel) {
+	fmt.Println("One success mail will be send from here")
 }
