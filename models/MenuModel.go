04e04d5481d7e69d4b3568fd5ceab5c4311436fc
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-18 16:38:07 +0600

    Menu model update for update

--- models/MenuModel.go
+++ models/MenuModel.go
@@ -6,13 +6,28 @@ import (
 )
 
 type MenuModel struct {
-	ID primitive.ObjectID `bson:"_id"`
+	ID primitive.ObjectID `bson:"_id" json:"id"`
 	//OrderId   string             `json:"orderId" validate:"required"`
-	Name      string    `json:"name" validate:"required"`
-	Category  string    `json:"category" validate:"required"`
-	MenuImage string    `json:"menuImage" validate:"required"`
-	StartDate string    `json:"startDate"`
-	EndDate   string    `json:"endDate"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	Name      string    `json:"name" validate:"required" bson:"name"`
+	Category  string    `json:"category" validate:"required" bson:"category"`
+	MenuImage string    `json:"menuImage" validate:"required" bson:"menuImage"`
+	StartDate string    `json:"startDate" bson:"startDate"`
+	EndDate   string    `json:"endDate" bson:"endDate"`
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+	IsActive  bool      `bson:"isActive" json:"isActive" validate:"required"`
+	CreateBy  string    `bson:"createBy" json:"createBy"`
+	UpdatedBy string    `bson:"updateBy" json:"updateBy"`
+}
+
+type UpdateMenuModel struct {
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	Name      string             `json:"name" validate:"required" bson:"name"`
+	Category  string             `json:"category" validate:"required" bson:"category"`
+	MenuImage string             `json:"menuImage" validate:"required" bson:"menuImage"`
+	StartDate string             `json:"startDate" bson:"startDate"`
+	EndDate   string             `json:"endDate" bson:"endDate"`
+	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
+	IsActive  bool               `bson:"isActive" json:"isActive" validate:"required"`
+	UpdatedBy string             `bson:"updateBy" json:"updateBy"`
 }
