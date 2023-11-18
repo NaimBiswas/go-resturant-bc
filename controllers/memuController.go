60d854d796f88ae28d1859f0e9ef1203613c9cd7
NaimBiswas <nayeembiswas2@gmail.com>
2023-11-18 16:37:27 +0600

    chore(UP, DEL): Menu Update and delete Func

--- controllers/memuController.go
+++ controllers/memuController.go
@@ -6,7 +6,11 @@ import (
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
+	"restaurent-mng-bc/commonServices"
+	"restaurent-mng-bc/commonType/collections"
 	"restaurent-mng-bc/database"
 	"restaurent-mng-bc/models"
 	"restaurent-mng-bc/response"
@@ -33,7 +37,8 @@ func CreateMenu(c *gin.Context) {
 	menu.ID = primitive.NewObjectID()
 	menu.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 	menu.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
-
+	menu.CreateBy = "Admin - RMS"
+	menu.UpdatedBy = "Admin - RMS"
 	//check the validation for title
 	count, _ := Db.Collection(menuCollection).CountDocuments(ctx, bson.M{"name": menu.Name})
 	if count > 0 {
@@ -81,10 +86,64 @@ func GetMenu(c *gin.Context) {
 	response.SuccessResponse(c, http.StatusAccepted, menu)
 }
 
-func UpdateMenu(ctx *gin.Context) {
+func UpdateMenu(c *gin.Context) {
+	var _Db = database.Db
+	var menuId = c.Param("id")
 
+	ObjectId, _ := primitive.ObjectIDFromHex(menuId)
+	redisKey := collections.MenuCollection + ":" + menuId
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var updateMenuModel models.UpdateMenuModel
+	var menuModel models.MenuModel
+	if err := c.BindJSON(&updateMenuModel); err != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	validateErr := commonServices.Validate.Struct(updateMenuModel)
+	if validateErr != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, validateErr.Error())
+		return
+	}
+
+	updateMenuModel.ID = ObjectId
+	updateMenuModel.UpdatedAt = commonServices.GetDate()
+	updateMenuModel.UpdatedBy = "Admin - RMS"
+
+	after := options.After
+	log.Println(updateMenuModel)
+	result := _Db.Collection(collections.MenuCollection).FindOneAndUpdate(ctx,
+		bson.M{"_id": ObjectId},
+		bson.M{"$set": updateMenuModel},
+		&options.FindOneAndUpdateOptions{ReturnDocument: &after})
+
+	if result.Err() != nil {
+		response.ErrorResponse(c, http.StatusBadRequest, result.Err().Error())
+		return
+	}
+	result.Decode(&menuModel)
+
+	response.SuccessResponse(c, http.StatusOK, menuModel)
+	commonServices.SetStringVal(ctx, redisKey, menuModel, 180)
 }
 
-func DeleteMenu(ctx *gin.Context) {
+func DeleteMenu(c *gin.Context) {
+	var _Db = database.Db
+	var menuId = c.Param("id")
 
+	ObjectId, _ := primitive.ObjectIDFromHex(menuId)
+	redisKey := collections.MenuCollection + ":" + menuId
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	_Db.Collection(collections.MenuCollection).FindOneAndUpdate(ctx, bson.M{"_id": ObjectId}, bson.M{"$set": bson.M{"isActive": false}})
+	err := commonServices.DeleteRedisStringValue(ctx, redisKey)
+	if err != nil {
+		log.Println("Error in Redis Delete", err.Error())
+	}
+	response.SuccessResponse(c, http.StatusOK, "Deleted Successfully")
 }
